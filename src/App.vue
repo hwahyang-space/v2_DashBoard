be10ca099516323b684793366fe1c50eb8187b02
HwaHyang <me@hwahyang.space>
2023-09-01 20:42:59 +0900

    feat: custom Scrollbar

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,33 @@ const isLoading = useLoaderState();
 	<PreloaderView v-if="isLoading.state" />
 	<RouterView />
 </template>
+
+<style>
+* {
+	scrollbar-width: thin;
+	scrollbar-color: #b268dd;
+}
+
+*::-webkit-scrollbar {
+	width: 7px;
+}
+
+*::-webkit-scrollbar-track {
+	background: transparent;
+}
+
+*::-webkit-scrollbar-thumb {
+	background: #b268dd;
+	border: 7px solid transparent;
+	border-radius: 10px;
+}
+
+*::-webkit-scrollbar-corner {
+	background: transparent;
+}
+
+html.scroll-hidden {
+	color: transparent;
+}
+</style>
+
