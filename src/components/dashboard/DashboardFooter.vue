c99555d650813aff4d71646784ea46c0398f0669
HwaHyang <me@hwahyang.space>
2023-09-10 14:36:18 +0900

    feat, design: added some links in footer

--- src/components/dashboard/DashboardFooter.vue
+++ src/components/dashboard/DashboardFooter.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
     <footer class="footer pt-4">
         <div class="container-fluid">
@@ -7,6 +11,22 @@
                         &copy; 2017-2023 <a href="https://hwahyang.space" target="_blank">HwaHyang</a>.
                         Design: <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>.
                     </div>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="nav nav-footer justify-content-center justify-content-lg-end Pretendard-Light">
+                        <li class="nav-item">
+                            <a href="https://hwahyang.space" class="nav-link text-muted" target="_blank">Homepage</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://pf.hwahyang.space" class="nav-link text-muted" target="_blank">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/" class="nav-link text-muted">Dashboard</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://github.com/hwahyang-space" class="nav-link pe-0 text-muted" target="_blank">Project GitHub</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
