5a29d85c2e8e23c6765e645a7d3fb5e086950ac2
HwaHyang <me@hwahyang.space>
2023-09-10 17:40:28 +0900

    feat, design: 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 
+import DashboardSidenavView from '@/components/dashboard/DashboardSidenav.vue';
+import DashboardHeaderView from '@/components/dashboard/DashboardHeader.vue';
+import DashboardFooterView from '@/components/dashboard/DashboardFooter.vue';
+
 document.body.className = 'g-sidenav-show bg-gray-100';
 </script>
 
 <template>
-	<p>404 NOT FOUND</p>
-	<p><RouterLink to="/">Go Home</RouterLink></p>
-	<p><a href="javascript:;" @click="$router.go(-1)">Go Back</a></p>
+	<DashboardSidenavView active="dashboard-root" />
+	<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+		<DashboardHeaderView title="Dashboard" />
+		<div class="container-fluid py-4">
+			<p>404 NOT FOUND</p>
+			<p><RouterLink to="/">Go Home</RouterLink></p>
+			<p><a href="javascript:;" @click="$router.go(-1)">Go Back</a></p>
+
+			<DashboardFooterView />
+		</div>
+	</main>
 </template>
