d1d0c2c99b87b38ca39ca81f7d7d039b6221e9c3
HwaHyang <me@hwahyang.space>
2023-09-10 14:35:29 +0900

    style: trivial

--- src/components/authentication/AuthenticationFooter.vue
+++ src/components/authentication/AuthenticationFooter.vue
@@ -4,9 +4,7 @@
 			<div class="row">
 				<div class="col-8 mx-auto text-center mt-1">
 					<p class="mb-0 text-secondary Pretendard-Regular">
-						&copy; 2017-2023
-						<a href="https://hwahyang.space" target="_blank">HwaHyang</a>. Design:
-						<a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>.
+						&copy; 2017-2023 <a href="https://hwahyang.space" target="_blank">HwaHyang</a>. Design: <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>.
 					</p>
 				</div>
 			</div>
